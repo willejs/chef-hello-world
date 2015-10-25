db0ad9bd26bdd82b0d02fe3b3df226a4a262f9fc
will.salt <williamejsalt@gmail.com>
2015-10-25 14:32:05 +0000

    added unit and integration tests

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -1,10 +1,28 @@
 require_relative '../spec_helper'
 
-describe 'backupserv::default' do
-  let(:chef_run) do
-    ChefSpec::ServerRunner.new do |node, server|
-      server.create_environment('dev', description: 'test env')
-      node.chef_environment = 'dev'
-    end.converge described_recipe
+describe 'hello-world::default' do
+  let(:chef_run) { ChefSpec::ServerRunner.converge(described_recipe) }
+  before do
+    stub_command("/usr/local/go/bin/go version | grep \"go1.5 \"").and_return(0)
   end
+
+  it 'installs the go-hello-world package' do
+    expect(chef_run).to install_golang_package('github.com/willejs/go-hello-world')
+  end
+
+  it 'creates the hello-world upstart config file' do
+    expect(chef_run).to create_template('/etc/init/hello-world.conf')
+    resource = chef_run.template('/etc/init/hello-world.conf')
+    expect(resource).to notify('service[hello-world]').to(:restart).delayed
+  end
+
+  it 'creates the helloworld user' do
+    expect(chef_run).to create_user('helloworld')
+  end
+
+  it 'installs & starts the go-hello-world upstart service' do
+    expect(chef_run).to enable_service('hello-world')
+    expect(chef_run).to start_service('hello-world')
+  end
+
 end
--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -1,1 +1,27 @@
 require_relative '../spec_helper'
+
+describe 'default' do
+  describe file('/opt/go/bin/go-hello-world') do
+    it { should exist }
+  end
+
+  describe file('/etc/init/hello-world.conf') do
+    it { should exist }
+  end
+
+  describe user('helloworld') do
+    it { should exist }
+  end
+
+  describe service('hello-world') do
+    it { should be_running }
+  end
+
+  describe port(8484) do
+    it { should be_listening.with('tcp6') }
+  end
+
+  describe command('wget -o /dev/null localhost:8484') do
+    its(:exit_status) { should eq 0 }
+  end
+end
